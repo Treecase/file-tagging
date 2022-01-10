79c266121dff4ce7a91bd706aaa26ad1100d4f81
Trevor Last <trevorclast@gmail.com>
2022-01-09 20:21:29 -0500

    Some input validation
    tags.json data is validated on read. JSON output is sorted on output.

--- filetagging/filetagging.py
+++ filetagging/filetagging.py
@@ -23,38 +23,41 @@ from pathlib import Path, PurePath
 
 
 class TagsFile:
-    """Tags file Context Manager."""
-    def __init__(self, directory: PurePath, create_new: bool=False) -> None:
-        """Create a tags Context Manager.
+    """Tags file Context Manager.
 
-        Keyword arguments:
-        create_new -- whether an error should be raised if the tags.json file
-            doesn't already exist (default False)
+    ``self.tags`` is a dict containing files and their associated tags.
+    """
+
+    def __init__(self, directory: str, create_new: bool=False) -> None:
+        """Create a tags file Context Manager for ``directory``.
+
+        ``create_new`` specifies whether a ``FileNotFoundError`` should be
+        raised if the tags.json file doesn't already exist (default ``False``).
         """
-        self.filepath = Path(directory/"tags.json")
-        try:
-            self.tags = {
-                k:set(v) for k,v in json.loads(self.filepath.read_text(encoding="utf-8")).items()
-            }
-        except FileNotFoundError as err:
+        self._filepath = Path(directory/"tags.json")
+        if self._filepath.exists():
+            self.tags = json.loads(self._filepath.read_text(encoding="utf-8"))
+        else:
             if create_new:
                 self.tags = {}
             else:
-                raise err
+                raise FileNotFoundError(
+                    f"Missing file '{directory}'")
+        self.tags = _validate_tags_data(self.tags)
 
     def get_tags(self, filename: str) -> set[str]:
         """Get the tags associated with a file.
 
-        filename should be a file NAME, not a file PATH. Strip the directory
-        off first!
+        ``filename`` should be a file NAME, not a file PATH. Strip the
+        directory off first!
         """
         return self.tags[filename]
 
     def set_tags(self, filename: str, tags: set[str]) -> None:
         """Set the tags associated with a file.
 
-        filename should be a file NAME, not a file PATH. Strip the directory
-        off first!
+        ``filename`` should be a file NAME, not a file PATH. Strip the
+        directory off first!
         """
         self.tags[filename] = set(tags)
         if not self.tags[filename]:
@@ -63,8 +66,8 @@ class TagsFile:
     def add_tag(self, filename: str, tag: str) -> None:
         """Add a tag to a file.
 
-        filename should be a file NAME, not a file PATH. Strip the directory
-        off first!
+        ``filename`` should be a file NAME, not a file PATH. Strip the
+        directory off first!
         """
         if filename in self.tags:
             self.tags[filename].add(tag)
@@ -72,10 +75,10 @@ class TagsFile:
             self.tags[filename] = {tag}
 
     def remove_tag(self, filename: str, tag: str) -> None:
-        """Delete a tag from a file.
+        """Remove a tag from a file.
 
-        filename should be a file NAME, not a file PATH. Strip the directory
-        off first!
+        ``filename`` should be a file NAME, not a file PATH. Strip the
+        directory off first!
         """
         self.tags[filename].discard(tag)
         if not self.tags[filename]:
@@ -87,21 +90,41 @@ class TagsFile:
     def __exit__(self, exc_type, exc_value, traceback):
         # save the tagsfile if the tags are not empty
         if self.tags:
-            with open(self.filepath, mode="w", encoding="utf-8") as file:
-                file.write(json.dumps(
+            self._filepath.write_text(
+                json.dumps(
                     {k:list(v) for k,v in self.tags.items()},
-                    indent=4))
+                    indent=4,
+                    sort_keys=True),
+                encoding="utf-8")
         # if the tags are empty, and the tagsfile exists, delete it
-        elif Path(self.filepath).exists():
-            Path(self.filepath).unlink()
+        elif self._filepath.exists():
+            self._filepath.unlink()
+
+
+def _validate_tags_data(data: dict) -> dict[str, set[str]]:
+    """Validate and return a formatted JSON dump.
+
+    ``data`` must be of format: ``dict[str, list[str]]``. If it does not match
+    this format, a ``TypeError`` will be raised.
+    """
+    for key,value in data.items():
+        if not isinstance(key, str):
+            raise TypeError(
+                "tags data contains key of non-string type "
+                + type(key).__name__)
+        if not isinstance(value, list):
+            raise TypeError(
+                f'"{key}" is of non-list type {type(value).__name__}')
+        if not all(isinstance(i, str) for i in value):
+            raise TypeError(f'"{key}" list contains non-string elements')
+    return {k:set(str(i) for i in v) for k,v in data.items()}
 
 
 def open_tags(filepath: str, create_new: bool=False) -> TagsFile:
-    """Open the tags file associated with the given filepath.
+    """Open the tags file associated with ``filepath``.
 
-    Keyword arguments:
-    create_new -- whether an error should be raised if the tags.json file
-        doesn't already exist (default False)
+    ``create_new`` specifies whether a ``FileNotFoundError`` should be raised
+    if the tags.json file doesn't already exist (default ``False``).
     """
     path = Path(filepath)
     if path.is_dir():
@@ -121,15 +144,15 @@ def ls_tags(filepath: str) -> None:
 def filter_tags(tag: str, directory: str=".") -> None:
     """Print all files that match the tag.
 
-    Keyword arguments:
-    directory -- The directory to filter in.
+    ``directory`` is the directory read the tags from. (default is the current
+    directory).
     """
     with open_tags(directory) as tags:
         matches = [
             file
             for (file,file_tags) in tags.tags.items()
             if tag in file_tags
-            ]
+        ]
         for match in matches:
             print(match)
 
